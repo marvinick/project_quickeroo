3cb2c1a528e7629cb47a49421871ce57c5618b93
Marvki <marvkipi@gmail.com>
2016-05-03 21:29:24 -0700

    skeletons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,10 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('yapp', [
-    'ui.router',
-    'ngAnimate'
-  ])
+  .module('yapp', ['ui.router','ngAnimate'])
   .config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
@@ -24,27 +21,27 @@ angular
         url: '',
         templateUrl: 'views/base.html'
       })
-        .state('login', {
-          url: '/login',
-          parent: 'base',
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl'
-        })
-        .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardCtrl'
-        })
-          .state('overview', {
-            url: '/overview',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
-          })
-          .state('reports', {
-            url: '/reports',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/reports.html'
-          });
+      .state('login', {
+        url: '/login',
+        parent: 'base',
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        parent: 'base',
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .state('overview', {
+        url: '/overview',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/overview.html'
+      })
+      .state('reports', {
+        url: '/reports',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/reports.html'
+      });
 
   });
