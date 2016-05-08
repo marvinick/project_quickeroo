bccfd9e28529ef05c306c2195d490f9829ea48ba
Marvki <marvkipi@gmail.com>
2016-05-08 16:47:40 -0700

    angularfire works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular
-  .module('yapp', ['ui.router','ngAnimate', 'firebase'])
+var app = angular.module('yapp', ['ui.router','ngAnimate', 'firebase'])
   .config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
@@ -36,4 +35,51 @@ angular
         templateUrl: 'views/dashboard/reports.html'
       });
 
+
+
   });
+
+// this factory returns a synchronized array of chat messages
+app.factory("chatMessages", ["$firebaseArray",
+  function($firebaseArray) {
+    // create a reference to the database location where we will store our data
+    var randomRoomId = Math.round(Math.random() * 100000000);
+    var ref = new Firebase("https://docs-sandbox.firebaseio.com/af/intro/demo/" + randomRoomId);
+
+    // this uses AngularFire to create the synchronized array
+    return $firebaseArray(ref);
+  }
+]);
+
+app.controller("ChatCtrl", ["$scope", "chatMessages",
+  // we pass our new chatMessages factory into the controller
+  function($scope, chatMessages) {
+    $scope.user = "Guest " + Math.round(Math.random() * 100);
+
+    // we add chatMessages array to the scope to be used in our ng-repeat
+    $scope.messages = chatMessages;
+
+    // a method to create new messages; called by ng-submit
+    $scope.addMessage = function() {
+      // calling $add on a synchronized array is like Array.push(),
+      // except that it saves the changes to our database!
+      $scope.messages.$add({
+        from: $scope.user,
+        content: $scope.message
+      });
+
+      // reset the message input
+      $scope.message = "";
+    };
+
+    // if the messages are empty, add something for fun!
+    $scope.messages.$loaded(function() {
+      if ($scope.messages.length === 0) {
+        $scope.messages.$add({
+          from: "Firebase Docs",
+          content: "Hello world!"
+        });
+      }
+    });
+  }
+]);
--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,14 +1,47 @@
-	'use strict';
+// define our app and dependencies (remember to include firebase!)
+var app = angular.module("sampleApp", ["firebase"]);
 
-	angular.module("quickeroo")
-	.controller("ProfileCtrl", function($scope, $firebaseObject) {
-		//connect to firebase 
-		var ref = new Firebase("https://quickeroo.firebaseio.com/");
-		var fb = $firebase(ref);
+// this factory returns a synchronized array of chat messages
+app.factory("chatMessages", ["$firebaseArray",
+  function($firebaseArray) {
+    // create a reference to the database location where we will store our data
+    var randomRoomId = Math.round(Math.random() * 100000000);
+    var ref = new Firebase("https://docs-sandbox.firebaseio.com/af/intro/demo/" + randomRoomId);
 
-		//sync as object
-		var syncObject = fb.$asObject();
+    // this uses AngularFire to create the synchronized array
+    return $firebaseArray(ref);
+  }
+]);
 
-		//three way data binding 
-		syncObject.$bindTo($scope, 'days');
-	});
+app.controller("ChatCtrl", ["$scope", "chatMessages",
+  // we pass our new chatMessages factory into the controller
+  function($scope, chatMessages) {
+    $scope.user = "Guest " + Math.round(Math.random() * 100);
+
+    // we add chatMessages array to the scope to be used in our ng-repeat
+    $scope.messages = chatMessages;
+
+    // a method to create new messages; called by ng-submit
+    $scope.addMessage = function() {
+      // calling $add on a synchronized array is like Array.push(),
+      // except that it saves the changes to our database!
+      $scope.messages.$add({
+        from: $scope.user,
+        content: $scope.message
+      });
+
+      // reset the message input
+      $scope.message = "";
+    };
+
+    // if the messages are empty, add something for fun!
+    $scope.messages.$loaded(function() {
+      if ($scope.messages.length === 0) {
+        $scope.messages.$add({
+          from: "Firebase Docs",
+          content: "Hello world!"
+        });
+      }
+    });
+  }
+]);
