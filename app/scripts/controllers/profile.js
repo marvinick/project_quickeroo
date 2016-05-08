39fc1bb2072edb34d2b31335db7b2313b802aa4f
Marvki <marvkipi@gmail.com>
2016-05-08 16:21:32 -0700

    new firebase url

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,14 +1,14 @@
-// 'use strict';
+	'use strict';
 
-// angular.module("quickeroo")
-// .controller("ProfileCtrl", function($scope, $firebaseObject) {
-// 	//connect to firebase 
-// 	var ref = new Firebase("https://sweltering-fire-6981.firebaseio.com/days");
-// 	var fb = $firebase(ref);
+	angular.module("quickeroo")
+	.controller("ProfileCtrl", function($scope, $firebaseObject) {
+		//connect to firebase 
+		var ref = new Firebase("https://quickeroo.firebaseio.com/");
+		var fb = $firebase(ref);
 
-// 	//sync as object
-// 	var syncObject = fb.$asObject();
+		//sync as object
+		var syncObject = fb.$asObject();
 
-// 	//three way data binding 
-// 	syncObject.$bindTo($scope, 'days');
-// });
+		//three way data binding 
+		syncObject.$bindTo($scope, 'days');
+	});
